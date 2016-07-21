dd58eb0057eea9889228a0ce1bea9a0fbe159b06
Alex Paczynski <apaczy@gmail.com>
2016-07-21 12:28:20 -0700

    Refactored App as a class-based component, moved YTSearch invocation to constructor

--- src/index.js
+++ src/index.js
@@ -1,28 +1,33 @@
-import React from 'react'; // Go into node_modules and get React module
+import React, { Component } from 'react'; // Go into node_modules and get React module
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-// When importing code that WE write, we have to give
-// a file reference to the file
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAUdKxC2if0h_n4iX5yo_GZBLVh_XpujGk';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-  console.log(data);
-});
+class App extends Component {
+  constuctor(props) {
+    super(props);
 
-// Create a new component. This component should produce
-// some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      // In ES6, can condense obj props with same key-value
+      // names from { name: name } to { name }
+      this.setState({ videos });
+      //console.log(data);
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
-// Take this component's generated HTML and put it
-// on the the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container')); 
 // Render component App and insert it into this already
 // existing element
@@ -30,3 +35,9 @@ ReactDOM.render(<App />, document.querySelector('.container'));
 // React Downwards data-flow: 
 // Most parent component fetches information and pushes
 // it to child components
+
+
+
+
+
+
